e3b4b8ca451fd98f62a9383fc3cdec21981a707b
Priyanka Eshwaroju <42073800+priyankaeshwaroju@users.noreply.github.com>
2023-08-29 12:41:25 +0100

    key props

--- pages/categories.js
+++ pages/categories.js
@@ -122,7 +122,9 @@ function Categories({ swal }) {
             <option value="">No parent category</option>
             {categories.length > 0 &&
               categories.map((category) => (
-                <option value={category._id}>{category.name}</option>
+                <option key={category._id} value={category._id}>
+                  {category.name}
+                </option>
               ))}
           </select>
         </div>
@@ -137,7 +139,7 @@ function Categories({ swal }) {
           </button>
           {properties.length > 0 &&
             properties.map((property, index) => (
-              <div className="flex gap-1 mb-2">
+              <div key={index} className="flex gap-1 mb-2">
                 <input
                   type="text"
                   value={property.name}
@@ -207,7 +209,7 @@ function Categories({ swal }) {
             )}
             {categories.length > 0 &&
               categories.map((category) => (
-                <tr>
+                <tr key={category._id}>
                   <td>{category.name}</td>
                   <td>{category?.parent?.name}</td>
                   <td>
